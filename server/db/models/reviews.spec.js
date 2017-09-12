1446c254c8b96c94697a140b6eecd8e40f8515b0
Tawsif Ahmed <Sif@Tawsifs-MacBook-Pro.local>
2017-09-12 15:25:39 -0400

    added review model and spec - broken spec

---
+++ server/db/models/reviews.spec.js
@@ -0,0 +1,21 @@
+/* global describe beforeEach it */
+const {expect} = require('chai')
+const db = require('../index')
+const Review = db.model('review')
+
+describe('Review model', () => {
+    beforeEach(() => {
+        return db.sync({force: true})
+    })
+
+    var review = Review.build({
+        text: 'sample review'
+    })
+
+    describe('attributes', () => {
+        review.save()
+            .then((review) => {
+                expect(review.text).to.be.equal('sample review')
+            })
+    }) // end Review attributes
+})// end Review model
