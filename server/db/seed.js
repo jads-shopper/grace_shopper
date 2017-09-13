a9b415683fac2fa1c7645f56265d7f474b77a6ea
Tawsif Ahmed <Sif@Tawsifs-MBP.fios-router.home>
2017-09-12 21:43:34 -0400

    fixed seed file

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,17 +1,16 @@
 if (process.env.NODE_ENV !== 'production') require('../../secrets')
-const User = require('./models/user')
-const Product = require('./models/products')
+const db = require('../db')
+const User = db.model('user')
+const Product = db.model('product')
+const Review = db.model('review')
 const Chance = require('chance')
 const chance = new Chance()
 
-//user seed
-const userFirstName = []
-const userLastName = []
-const userEmail = []
-const password = []
-const isAdmin = []
-
 const promises = []
+const promisesReviews = []
+
+//user seed
+const userFirstName = [], userLastName = [], userEmail = [], password = [], isAdmin = []
 
 for (var i = 0; i < 50; i++) {
 	userFirstName.push(chance.first())
@@ -25,38 +24,60 @@ for (var i = 0; i < 50; i++) {
 userFirstName.map(( val, idx ) => {
 	promises.push(User.create({
 		firstName: val,
-		lastName: userLastName[idx],
-		email: userEmail[idx],
-		password: password[idx],
-		isAdmin: isAdmin[idx],
+		lastName : userLastName[idx],
+		email    : userEmail[idx],
+		password : password[idx],
+		isAdmin  : isAdmin[idx],
 	}))
 })
 // Product  seed
-const name = [], category = [] , imageUrl = [] , price  = [] , description  = [], quantity = [], isActive = []
+const name = [], category = [], imageUrl = [], price = [], description = [], quantity = [], isActive = []
 
 for (i = 0; i < 50; i++) {
 	name.push(chance.word())
 	category.push(chance.word())
 	imageUrl.push(chance.avatar())
-	price.push(chance.floating({fixed:2, min: 0.01, max: 1000}))
+	price.push(chance.floating({ fixed: 2, min: 0.01, max: 1000 }))
 	description.push(chance.paragraph())
-	quantity.push(chance.integer({min:0 , max: 50}))
+	quantity.push(chance.integer({ min: 0, max: 50 }))
 	isActive.push(chance.bool())
 }
 
-name.map((val, idx) => {
+name.map(( val, idx ) => {
 	promises.push(Product.create({
-		name: name[idx],
-		category: category[idx],
-		imageUrl: imageUrl[idx],
-		price: price[idx],
+		name       : name[idx],
+		category   : category[idx],
+		imageUrl   : imageUrl[idx],
+		price      : price[idx],
 		description: description[idx],
-		quantity: quantity[idx],
-		isActive: isActive[idx],
+		quantity   : quantity[idx],
+		isActive   : isActive[idx],
 	}))
 })
 
+// review seed
+const text = [], rating = [], productId = []
+
+for (i = 0; i < 50; i++) {
+	text.push(chance.paragraph())
+	rating.push(chance.integer({ min: 1, max: 5 }))
+	productId.push(chance.integer({ min: 1, max: 50 }))
+}
 
 
-Promise.all(promises).then(console.log)
+text.map(( val, idx ) => {
+	promisesReviews.push(Review.create({
+		text  : text[idx],
+		rating: rating[idx],
+		productId: productId[idx],
+	})
+	)
+	Promise.all(promises).then(() => {
+		// console.log(promisesReviews)
+		return Promise.all(promisesReviews)
+	})
+		.then(console.log('success'))
+		.catch(console.error)
+
+})
 
