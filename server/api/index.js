36f944a3b38f9a2b5623acad69a8444cf2fa9304
Anton Landauer <anton.landauer@gmail.com>
2017-09-14 11:20:08 -0400

    reviews models

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router()
 module.exports = router
 
+router.use('/reviews', require('./reviews'))
 router.use('/users', require('./users'))
 router.use('/orders', require('./orders'))
 router.use('/products', require('./products'))
-router.use('/reviews', require('./review'))
+router.use(( req, res, next ) => {
 
-router.use((req, res, next) => {
 	const error = new Error('Not Found')
 	error.status = 404
+
 	next(error)
 })
--- server/api/reviews.js
+++ server/api/reviews.js
@@ -1,26 +1,62 @@
 const router = require('express').Router()
 const { Review } = require('../db/models')
-module.exports = router
 
-router.get('/', ( req, res, next ) => {Review.findAll().then(res.json).catch(console.error)})
-
-router.get('/:id', ( req, res, next ) => {
+//get product reviews
+router.get('/product/:id', ( req, res, next ) => {
 	const { id } = req.params
 	Review.findById(id)
 	      .then(review => review ? res.json(review) : res.sendStatus(404))
 })
 
 
-router.post('/newReview/', ( req, res, next ) => {
-	const {userId, productId , text , stars}  = req.body
-
+// new review
+router.post('/new/', ( req, res, next ) => {
+	const { userId, productId, text, rating } = req.body
 	Review.create({
-		text: text,
-
-	})
-
-
+		text     : text,
+		rating   : rating,
+		ProductId: productId,
+		userId   : userId,
+	}).then(res.json)
 
 })
 
+// update review
+router.put('/update/', ( req, res, next ) => {
+	const { reviewId, text, rating } = req.body
+	Review.findById(reviewId)
+	      .then(( review ) => {
+		      return review.update({
+			      text  : text,
+			      rating: rating,
+		      }).then(res.json)
+		                   .catch(next)
+	      })
+})
 
+// delete review
+router.delete('/delete/:id', ( req, res, next ) => {
+	const { id } = req.params
+	Review.findOne({
+		where: {
+			id: {
+				$eq: id
+			}
+		}
+	}).then(task => task.destroy({ force: true }))
+})
+
+// find all user reviews
+router.get('/user/:id', ( req, res, next ) => {
+	const { id } = req.params
+	Review.findAll({
+		where: {
+			userId: {
+				$eq: id
+			}
+		}
+	}).then(res.json)
+	      .catch(next)
+})
+
+module.exports = router
