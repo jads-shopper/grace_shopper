69f87126d24e9ad9331c11b2b14551a0849090ff
Jackson Sui <kiro705@gmail.com>
2017-09-19 15:16:10 -0400

    refactored the admin edit order page

--- client/store/orders.js
+++ client/store/orders.js
@@ -5,6 +5,7 @@ import history from '../history'
  * ACTION TYPES
  */
 const GET_ORDERS = 'GET_ORDERS'
+const GET_SINGLE_ORDER = 'GET_SINGLE_ORDER'
 const POST_ORDER = 'POST_ORDER'
 const EDIT_ORDER = 'EDIT_ORDER'
 const DELETE_ORDER = 'DELETE_ORDER'
@@ -36,6 +37,17 @@ export function fetchOrdersUser (userId) {
 			.then(res => res.data)
 			.then(orders => {
 				dispatch(getOrders(orders))
+			})
+	}
+}
+
+export function fetchSingleOrder (orderId) {
+
+	return function thunk (dispatch) {
+		return axios.get(`/api/orders/${orderId}`)
+			.then(res => res.data)
+			.then(order => {
+				dispatch(getOrders(order))
 			})
 	}
 }
